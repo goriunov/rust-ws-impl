8ed806f28e5fe2de937128c2fd1a272ff64526ec
Dmitrii Goriunov <goriunovd@gmail.com>
2018-08-19 22:07:09 +1200

    Added handshake

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+extern crate base64;
 extern crate fnv;
 extern crate http_muncher;
 extern crate mio;
+extern crate sha1;
 
+use base64::encode;
 use fnv::FnvHashMap;
 use http_muncher::{Parser, ParserHandler};
 use mio::tcp::{Shutdown, TcpListener, TcpStream};
 use mio::*;
 use std::io::{Read, Write};
+
+fn generate_key(key: &String) -> String {
+    let mut m = sha1::Sha1::new();
+    m.update(key.as_bytes());
+    m.update("258EAFA5-E914-47DA-95CA-C5AB0DC85B11".as_bytes());
+    encode(&m.digest().bytes()).to_string()
+}
 
 struct HttpHeaders {
     headers: FnvHashMap<String, String>,
@@ -37,7 +47,15 @@ impl ParserHandler for HttpHeaders {
     }
 }
 
+enum WebsocketState {
+    AwaitingHandshake,
+    HandshakeResponse,
+    Connected,
+    Closed,
+}
+
 struct WebSocketClient {
+    state: WebsocketState,
     socket: TcpStream,
     interest: Ready,
     read_buf: Vec<u8>,
@@ -50,10 +68,27 @@ impl WebSocketClient {
         WebSocketClient {
             socket,
             hangs: 0,
+            state: WebsocketState::AwaitingHandshake,
             headers: FnvHashMap::default(),
             interest: Ready::readable(),
             read_buf: Vec::with_capacity(1024),
         }
+    }
+
+    fn write(&mut self) {
+        let response_key = generate_key(&self.headers.get("Sec-WebSocket-Key").unwrap());
+        let response = format!(
+            "HTTP/1.1 101 Switching Protocols\r\n\
+             Connection: Upgrade\r\n\
+             Sec-WebSocket-Accept: {}\r\n\
+             Upgrade: websocket\r\n\r\n",
+            response_key
+        );
+
+        self.socket.write(response.as_bytes()).unwrap();
+        self.state = WebsocketState::Connected;
+        self.interest.remove(Ready::writable());
+        self.interest.insert(Ready::readable());
     }
 
     fn read(&mut self) {
@@ -85,15 +120,22 @@ impl WebSocketClient {
                         parser.parse(&mut http_headers, &self.read_buf[..]);
                         self.headers = http_headers.get_all_headers();
 
+                        // print all headers
                         println!("{:#?}", self.headers);
-                    }
 
-                    // println!("{:?}", std::str::from_utf8(&self.read_buf).unwrap());
+                        // check if we have upgrade request
+                        match self.headers.get(&"Upgrade".to_string()) {
+                            Some(value) => if value == "websocket" {
+                                self.state = WebsocketState::HandshakeResponse;
+                                self.interest.remove(Ready::readable());
+                                self.interest.insert(Ready::writable());
+                                break;
+                            },
+                            None => {}
+                        }
+                    }
                 }
-                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                    // println!("Got in here with error");
-                    break;
-                }
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
                 Err(e) => {
                     println!("Error while reading socket: {:?}", e);
                     return;
@@ -165,17 +207,21 @@ impl WebsocketServer {
                             let client = self.clients.remove(&token).unwrap();
                             client.socket.shutdown(Shutdown::Both).unwrap();
                             poll.deregister(&client.socket).unwrap();
-                        } else if readiness.is_readable() {
+                        } else if readiness.is_writable() || readiness.is_readable() {
                             let mut client = self.clients.get_mut(&token).unwrap();
-                            client.read();
+
+                            if readiness.is_writable() {
+                                client.write()
+                            } else {
+                                client.read();
+                            }
+
                             poll.reregister(
                                 &client.socket,
                                 token,
                                 client.interest,
                                 PollOpt::edge() | PollOpt::oneshot(),
                             ).unwrap();
-                        } else if readiness.is_writable() {
-
                         }
                     }
                 }
